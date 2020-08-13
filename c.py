c16d3093b832348a1d47638e9a905831b3df5cc6
omar <omar_omarsalama@yahoo.com>
2020-08-13 22:19:50 +0200

    added lists

--- c.py
+++ c.py
@@ -38,14 +38,45 @@ x = 25
 
 def function5():
     global x
-    x =  5
+    x = x + 5
     print("this is inside the function",x)
     return "hello"
+
 function5()
-x = 10
 print("this is outside the function",x)
 
-# def function1():
-#     print("hello")
 
-# function1()
+
+
+x_list = [1, 2, 3, 4, 5, 6]
+print(x_list)
+
+x_list[5] = "something"
+print(x_list)
+
+x_list.append('newthing')
+print(x_list)
+
+x_list.insert(2, "something at index 2")
+print(x_list)
+
+x_list.remove(3)
+x_list.insert( 3,"III" )
+print(x_list)
+
+print(len(x_list)) # number of elements
+
+
+len_list_x = len(x_list)
+
+x_list.insert(len_list_x, 'new thing at the end')
+print(x_list)
+
+len_list_x = len(x_list)
+print(x_list[len_list_x - 1]) # -1 because the len gives you the number of elements but indecies start from 0
+
+new_tuple = (1, 2, 3, 5, 6, "something", [1, 3, 5])
+
+print(type(new_tuple[5]))
+
+print(new_tuple)
