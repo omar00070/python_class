cc2c3cdeb7f3d80058b09f53b617980f91a36a42
omar <omar_omarsalama@yahoo.com>
2020-08-13 21:46:01 +0200

    functions lesson

---
+++ c.py
@@ -0,0 +1,51 @@
+# def hello(name):
+#     name = name.capitalize()
+#     if name == "": return "hello world"
+#     return "hello " + name
+
+
+# f'hello {name}'
+
+
+# def function1():
+#     return 3
+
+# def function2():
+#     print(4)
+
+# x = function1()
+# y = function2()
+
+
+# print("this is x",x, "this is y", y)
+
+
+# def AreaCircle(r):
+#     area = r**2 * 22/7
+#     return area
+
+
+
+
+# def function3():
+#     return 10
+#     return 20
+#     print(1922)
+
+# print(function3())
+x = 25
+
+
+def function5():
+    global x
+    x =  5
+    print("this is inside the function",x)
+    return "hello"
+function5()
+x = 10
+print("this is outside the function",x)
+
+# def function1():
+#     print("hello")
+
+# function1()
