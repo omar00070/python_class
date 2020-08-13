81df0e4215cf96e60ead18782ef8ae6c3d489a96
omar <omar_omarsalama@yahoo.com>
2020-08-13 22:43:09 +0200

    lops

---
+++ loops.py
@@ -0,0 +1,24 @@
+# for anyvar in range(5):
+#     print(anyvar)
+
+x = 5 
+x = x + 1
+x += 1 # x = x + 1
+
+
+my_list = [1, 3, 4, 5, 6, 7]
+
+
+# my_list[0]= my_list[0] + 1
+len_list = len(my_list)
+
+# for i in range(len_list):
+#     my_list[i] = my_list[i] + 1
+
+
+# for i in range(len(my_list)): #put len(my_lis) inside range()
+#     print(my_list[i])
+
+
+for i in range(2, 20, 2):
+    print(i)
