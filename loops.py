1a230d64a7dc63ef15b502c9cf8841c24a035e6b
omar <omar_omarsalama@yahoo.com>
2020-08-17 21:41:52 +0200

    more to loops

--- loops.py
+++ loops.py
@@ -1,24 +1,72 @@
-# for anyvar in range(5):
+# # for anyvar in range(5):
+# #     print(anyvar)
+
+# x = 5 
+# x = x + 1
+# x += 1 # x = x + 1
+
+
+# my_list = [1, 3, 4, 5, 6, 7]
+
+
+# # my_list[0]= my_list[0] + 1
+# len_list = len(my_list)
+
+# # for i in range(len_list):
+# #     my_list[i] = my_list[i] + 1
+
+
+# # for i in range(len(my_list)): #put len(my_lis) inside range()
+# #     print(my_list[i])
+
+
+# for i in range(2, 20, 2):
+#     print(i)
+
+
+
+# for anyvar in range(1, 10, 2): #range(start, end, step)
 #     print(anyvar)
 
-x = 5 
-x = x + 1
-x += 1 # x = x + 1
 
 
-my_list = [1, 3, 4, 5, 6, 7]
+# for var in iterable:
+    #do something
+
+my_list = [1, 'adri', "omar", 4 ,5 ,6, "string"]
+
+# print(my_list[1])
 
 
-# my_list[0]= my_list[0] + 1
-len_list = len(my_list)
 
-# for i in range(len_list):
-#     my_list[i] = my_list[i] + 1
-
-
-# for i in range(len(my_list)): #put len(my_lis) inside range()
+# for i in range(7):
 #     print(my_list[i])
 
 
-for i in range(2, 20, 2):
-    print(i)
+
+
+# make a list , let it have 4 strings at least, the number of elements in the list is 10
+# and print everything in the list
+
+x = 10
+
+
+my_new_list = [-5, 1, 30, 100, 'max', 1, 20, -19]
+my_max = my_new_list[0]
+
+for i in range(len(my_new_list)): #my_max = -5
+    # check if there is any number bigger than my-max
+    # if any number is bigger, my_max is gonna be that number
+    if type(my_new_list[i]) == int:
+        if my_new_list[i] > my_max: # -5 > -5:
+            my_max = my_new_list[i]
+    print(my_max) # -5
+print(my_max)
+
+
+def myfunction(sugar, coffe, water): #function to make coffe
+    # heat the water
+    # add the water to the cup
+    # add the sugar
+    # add the coffe
+    return cup_of_coffe
